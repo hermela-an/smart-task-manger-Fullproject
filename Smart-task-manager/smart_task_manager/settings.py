9652f342bcf8245f583163b572eebcb201dc626b
Hermela Anteneh <hermelaanteneh21@gmail.com>
2026-02-10 13:34:29 +0300

    fix: use django static tags for robust production asset serving

--- Smart-task-manager/smart_task_manager/settings.py
+++ Smart-task-manager/smart_task_manager/settings.py
@@ -130,14 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR.parent / 'SmartTaskManagerFrontend',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 REST_FRAMEWORK = {
